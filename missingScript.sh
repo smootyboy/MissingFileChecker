02361bd217eaab7e06cdfc7d14a14df991d7f81a
Robert Jansma <r.jansma.nl@gmail.com>
2018-01-03 18:11:23 +0100

    Added usage error message

--- missingScript.sh
+++ missingScript.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+if [[ "$#" -ne 4 ]] ; then
+	echo "Usage: ./missingScript originalDir destinationDir missingDir tmp"
+	exit 1
+fi
+
 originalDir=$1 
-echo "$1"
 destinationDir=$2
-echo "$2"
 missingDir=$3
-echo "$3"
 tmp=$4
-echo "$4"
 
 find "$1" -type f -exec basename {} \; | cut -d/ -f2- | sort > "$tmp"
 
