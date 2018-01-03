9925ef239a1120aa5d773d1b328b853af70608ec
Robert Jansma <r.jansma.nl@gmail.com>
2018-01-03 18:48:27 +0100

    Added input checks

--- missingScript.sh
+++ missingScript.sh
@@ -10,6 +10,32 @@ destinationDir=$2
 missingDir=$3
 tmp=$4
 
+if [ ! -d "$originalDir" ]; then
+	echo "originalDir (arg 1) is not an existing directory"
+	exit 1
+fi
+if [ ! -d "$destinationDir" ]; then
+	echo "destinationDir (arg 2) is not an existing directory"
+	exit 1
+fi
+if [ ! -d "$missingDir" ]; then
+	echo "missingDir (arg 3) is not an existing directory"
+	echo "creating missingDir"
+	mkdir "$missingDir"
+fi
+if [ -e "$file" ]; then
+    echo "$tmp already exists"
+    read -p "Do you wish to overwrite? " -n 1 -r
+	echo    
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+    	echo "Continuing program"
+	else
+		exit 1
+	fi
+fi
+
+
 find "$1" -type f -exec basename {} \; | cut -d/ -f2- | sort > "$tmp"
 
 #directory_path="/Volumes/Seagate Exp/foto archief/"
